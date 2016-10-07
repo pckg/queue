3c2fe778ba1adc3a0cc206668f9c2f9276d07413
Bojan Rajh <schtr4jh@schtr4jh.net>
2016-10-07 18:04:55 +0200

    Test charts

--- src/Pckg/Queue/Provider/Queue.php
+++ src/Pckg/Queue/Provider/Queue.php
@@ -16,6 +16,7 @@ class Queue extends Provider
     public function assets()
     {
         return [
+            'bower_components/chart.js/dist/Chart.min.js',
             'footer' => [
                 'js/index.js',
             ],
