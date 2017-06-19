766983f97eb77dbc2dd5143d3622ce0db13d7972
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-06-19 12:30:12 +0200

    Queue timeout

--- src/Pckg/Queue/Console/RunQueue.php
+++ src/Pckg/Queue/Console/RunQueue.php
@@ -1,9 +1,9 @@
 <?php namespace Pckg\Queue\Console;
 
-use Exception;
 use Pckg\Framework\Console\Command;
 use Pckg\Queue\Record\Queue as QueueRecord;
 use Pckg\Queue\Service\Queue;
+use Symfony\Component\Process\Process;
 use Throwable;
 
 class RunQueue extends Command
@@ -29,8 +29,7 @@ class RunQueue extends Command
             function(QueueRecord $queue) {
                 $this->output('#' . $queue->id . ': ' . 'started (' . date('Y-m-d H:i:s') . ')');
                 $queue->changeStatus('started');
-            },
-            false
+            }
         );
 
         /**
@@ -42,94 +41,63 @@ class RunQueue extends Command
                 $queue->changeStatus('running');
 
                 $this->output('#' . $queue->id . ': ' . $queue->command);
+
                 $output = null;
-                $sha1Id = sha1($queue->id);
+                $error = null;
+
+                $command = $queue->command;
+                $exception = null;
+
+                /**
+                 * Run process
+                 */
                 try {
-                    $timeout = strtotime($queue->execute_at) - time();
-                    $command = $queue->command . ' && echo ' . $sha1Id;
-                    $lastLine = null;
-                    if (false && $timeout > 0) {
-                        exec('timeout -k 60 ' . $timeout . ' ' . $command, $output);
+                    $process = new Process($command);
+                    $process->setTimeout(60);
+                    $process->mustRun();
 
-                    } else {
-                        if (strpos($command, 'furs:')) {
-                            $command = str_replace(
-                                [
-                                    '/www/schtr4jh/derive.foobar.si/htdocs/',
-                                    '/www/schtr4jh/beta.derive.foobar.si/htdocs/',
-                                ],
-                                '/www/schtr4jh/bob.pckg.derive/htdocs/',
-                                $command
-                            );
-
-                            $connection = ssh2_connect(config('furs.sship'), 22);
-                            ssh2_auth_password($connection, config('furs.sshuser'), config('furs.sshpass'));
-
-                            $stream = ssh2_exec($connection, $command);
-
-                            $errorStream = ssh2_fetch_stream($stream, SSH2_STREAM_STDERR);
-
-                            stream_set_blocking($errorStream, true);
-                            stream_set_blocking($stream, true);
-
-                            $errorStreamContent = stream_get_contents($errorStream);
-                            $streamContent = stream_get_contents($stream);
-
-                            $output = $errorStreamContent . "\n" . $streamContent;
-                            $lastLine = substr($streamContent, -41, 40);
-
-                        } else {
-                            /**
-                             * @T00D00 - execute in current scope if possible, this will optimize things a little bit.
-                             */
-                            exec($command, $output);
-                            $lastLine = end($output);
-
-                        }
-
-                    }
-
-                    if ($lastLine != $sha1Id) {
-                        $queue->changeStatus(
-                            'failed_permanently',
-                            [
-                                'log' => 'FAILED: ' . (is_string($output) ? $output : implode("\n", $output)),
-                            ]
-                        );
-
-                        return;
-
-                        throw new Exception('Job failed');
-                    }
+                    $output = $process->getOutput();
+                    $error = $process->getErrorOutput();
                 } catch (Throwable $e) {
+                    $exception = $e;
+                }
+
+                /**
+                 * Handle output log.
+                 */
+                if ($output) {
+                    $queue->createLog(['log' => 'Output: ' . $output]);
+                }
+
+                /**
+                 * Handle error log.
+                 */
+                if ($error) {
+                    $queue->createLog(['log' => 'Error: ' . $error]);
+                }
+
+                /**
+                 * Handle exception
+                 */
+                if ($exception) {
+                    $this->output('#' . $queue->id . ': ' . 'failed permanently (' . date('Y-m-d H:i:s') . ')');
+
                     $queue->changeStatus(
                         'failed_permanently',
                         [
-                            'log' => exception($e),
+                            'log' => exception($exception),
                         ]
                     );
+                } else {
+                    $this->output('#' . $queue->id . ': ' . 'finished (' . date('Y-m-d H:i:s') . ')');
 
-                    return;
-                }
-
-                if (!$output) {
                     $queue->changeStatus(
-                        'failed_permanently',
+                        'finished',
                         [
-                            'log' => 'No output',
+                            'log' => $output,
                         ]
                     );
-
-                    return;
                 }
-
-                $this->output('#' . $queue->id . ': ' . 'finished (' . date('Y-m-d H:i:s') . ')');
-                $queue->changeStatus(
-                    'finished',
-                    [
-                        'log' => is_string($output) ? $output : implode("\n", $output),
-                    ]
-                );
             },
             false
         );
