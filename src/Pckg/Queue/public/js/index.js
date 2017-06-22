94b190888e81f59d02b92c574802bcd9f65baf73
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-06-22 23:28:29 +0200

    Fix

--- src/Pckg/Queue/public/js/index.js
+++ src/Pckg/Queue/public/js/index.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
 
-    var ctx = document.getElementById("myChart");
-    var myChart = new Chart(ctx, {
-        type: 'line',
-        data: $('#myChart').data('chartjs')
-    });
+    if ($('#myChart').length) {
+        var ctx = document.getElementById("myChart");
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: $('#myChart').data('chartjs')
+        });
+    }
 
 });
