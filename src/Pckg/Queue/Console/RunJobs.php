ca45213298282c525ce4d0ff492501bf71aab0a3
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-06-22 11:34:20 +0200

    PID changes

--- src/Pckg/Queue/Console/RunJobs.php
+++ src/Pckg/Queue/Console/RunJobs.php
@@ -5,15 +5,21 @@ use Pckg\Framework\Console\Command;
 use Pckg\Manager\Job as JobManager;
 use Pckg\Queue\Service\Cron\Job;
 use Pckg\Queue\Service\Queue;
+use Symfony\Component\Console\Input\InputOption;
 use Throwable;
 
 class RunJobs extends Command
 {
 
+    protected $pidFile;
+
     protected function configure()
     {
         $this->setName('cron:run')
-             ->setDescription('Run cronjobs / jobs');
+             ->setDescription('Run cronjobs / jobs')
+             ->addOptions([
+                              'pid-file' => 'Process ID file for updates',
+                          ], InputOption::VALUE_OPTIONAL);
     }
 
     /**
@@ -21,31 +27,68 @@ class RunJobs extends Command
      */
     public function handle()
     {
-        $this->output('Starting cronjob at ' . date('Y-m-d H:i:s'));
-        $jobs = new Collection(context(JobManager::class)->all());
-        $jobs->each(
-            function(Job $job) {
-                if ($job->shouldBeRun()) {
-                    $this->output('Running ' . $job->getCommand());
-                    try {
-                        $process = $job->run();
-                        /**
-                         * @T00D00 - log output and error output
-                         */
-                        if ($output = $process->getOutput()) {
-                            $this->output("Output: " . $output);
-                        }
+        $this->pidFile = $this->option('pid-file');
+        $this->touchPidFile();
 
-                        if ($errorOutput = $process->getErrorOutput()) {
-                            $this->output("Error:" . $errorOutput);
+        $jobs = new Collection(context(JobManager::class)->all());
+        $e = null;
+
+        try {
+            $jobs->each(
+                function(Job $job) {
+                    /**
+                     * Touch file so parent process knows that we're not stuck.
+                     */
+                    $this->touchPidFile();
+
+                    /**
+                     * Check if it's correct time of day to be run.
+                     */
+                    if ($job->shouldBeRun()) {
+                        $e = null;
+                        $process = null;
+
+                        try {
+                            $process = $job->run();
+                        } catch (Throwable $e) {
+                            $this->output("Exception: " . exception($e));
+                        } finally {
+                            /**
+                             * @T00D00 - log output and error output
+                             */
+                            if ($process && $output = $process->getOutput()) {
+                                $this->output("Output: " . $output);
+                            }
+
+                            if ($process && $errorOutput = $process->getErrorOutput()) {
+                                $this->output("Error:" . $errorOutput);
+                            }
                         }
-                    } catch (Throwable $e) {
-                        $this->output("Exception: " . exception($e));
                     }
                 }
+            );
+        } catch (Throwable $e) {
+        } finally {
+            $this->removePidFile();
+
+            if ($e) {
+                throw $e;
             }
-        );
-        $this->output('Finished cronjob at ' . date('Y-m-d H:i:s'));
+        }
+    }
+
+    protected function touchPidFile()
+    {
+        if ($this->pidFile) {
+            file_put_contents($this->pidFile, "running");
+        }
+    }
+
+    protected function removePidFile()
+    {
+        if ($this->pidFile) {
+            file_put_contents($this->pidFile, "ok");
+        }
     }
 
 }
