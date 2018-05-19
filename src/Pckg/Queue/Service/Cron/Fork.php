cc056206e42c43f20f4a5fa28e301ea7634deac1
Bojan Rajh <schtr4jh@schtr4jh.net>
2018-05-19 18:56:36 +0200

    Wait for forked

--- src/Pckg/Queue/Service/Cron/Fork.php
+++ src/Pckg/Queue/Service/Cron/Fork.php
@@ -3,6 +3,70 @@
 class Fork
 {
 
+    /**
+     * @var array
+     */
+    protected static $pids = [];
+
+    /**
+     * @param $pid
+     */
+    public static function waitFor($pid)
+    {
+        static::$pids[$pid] = $pid;
+    }
+
+    /**
+     *
+     */
+    public static function waitWaiting()
+    {
+        while (count(static::$pids) > 0) {
+            /**
+             * Wait for 5 seconds before next check.
+             */
+            sleep(5);
+
+            foreach (static::$pids as $pid) {
+                /**
+                 * Get process status code.
+                 */
+                $res = pcntl_waitpid($pid, $status, WNOHANG);
+
+                if (!($res > 0)) {
+                    /**
+                     * Process is still running, check another process.
+                     */
+                    continue;
+                }
+
+                /**
+                 * Process has ended, remove it from watch list.
+                 */
+                unset(static::$pids[$pid]);
+            }
+
+            /**
+             * We have processes to check, retry.
+             */
+            if (static::$pids) {
+                continue;
+            }
+
+            /**
+             * All waiting processes has finished, quit.
+             */
+            break;
+        }
+    }
+
+    /**
+     * @param callable      $fork
+     * @param callable|null $name
+     * @param callable|null $error
+     *
+     * @return bool|int
+     */
     public static function fork(callable $fork, callable $name = null, callable $error = null)
     {
         $pid = pcntl_fork();
