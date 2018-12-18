5c140256ca5aa27f7715ddb8cee9a08cfc65f1ad
Bojan Rajh <schtr4jh@schtr4jh.net>
2018-12-18 20:03:27 +0100

    Fork

--- src/Pckg/Queue/Service/Cron/Fork.php
+++ src/Pckg/Queue/Service/Cron/Fork.php
@@ -66,12 +66,55 @@ class Fork
      *
      * @return bool|int
      */
-    public static function fork(callable $fork, callable $name = null, callable $error = null)
+    public static function fork(callable $fork, callable $name = null, callable $error = null, &$sockets = [])
     {
+        /**
+         * Create a communication socket chanel.
+         */
+        if (socket_create_pair(AF_UNIX, SOCK_STREAM, 0, $sockets) === false) {
+            throw new \Exception('socket_create_pair() failed. Reason: ' . socket_strerror(socket_last_error()));
+        }
+
+        /**
+         * Test msg.
+         */
+        $strone = 'String one';
+        $strtwo = 'String two';
+
+        /**
+         * Try to fork process.
+         */
         $pid = pcntl_fork();
         if ($pid == -1 || $pid) {
+            /**
+             * Failed or success, return to parent.
+             */
+
+            socket_close($sockets[0]);
+            if (socket_write($sockets[1], $strone, strlen($strone)) === false) {
+                echo "parent socket_write() failed. Reason: ".socket_strerror(socket_last_error($ary[1]));
+            }
+            if (socket_read($sockets[1], strlen($strtwo), PHP_BINARY_READ) == $strtwo) {
+                echo "parent Recieved $strtwo\n";
+            }
+            socket_close($sockets[1]);
+
             return $pid;
         }
+
+        /**
+         * Allow child to communicate to parent.
+         */
+        $message = function($string) use (&$sockets, $strtwo, $strone){
+            socket_close($sockets[1]);
+            if (socket_write($sockets[0], $string, strlen($string)) === false) {
+                echo "child socket_write() failed. Reason: " . socket_strerror(socket_last_error($ary[0]));
+            }
+            if (socket_read($sockets[0], strlen($strone), PHP_BINARY_READ) == $strone) {
+                echo "child Recieved $strone\n";
+            }
+            socket_close($ary[0]);
+        };
 
         /**
          * First, change process name.
@@ -93,7 +136,14 @@ class Fork
              */
             trigger('forked');
 
-            $fork();
+            /**
+             * Execute forked code.
+             */
+            $fork($message);
+
+            /**
+             * How to communicate status to parent?
+             */
 
             exit(0);
         } catch (Throwable $e) {
