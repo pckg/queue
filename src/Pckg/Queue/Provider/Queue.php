51d1407509665d35c5e20450a0927eb19dca8296
Bojan Rajh <schtr4jh@schtr4jh.net>
2019-11-14 16:52:52 +0100

    Cleanup

--- src/Pckg/Queue/Provider/Queue.php
+++ src/Pckg/Queue/Provider/Queue.php
@@ -1,63 +1,13 @@
 <?php namespace Pckg\Queue\Provider;
 
-use Pckg\Auth\Provider\Auth as AuthProvider;
-use Pckg\Dynamic\Provider\Dynamic as DynamicProvider;
 use Pckg\Framework\Provider;
-use Pckg\Generic\Provider\Generic as GenericProvider;
-use Pckg\Manager\Provider\Manager as ManagerProvider;
 use Pckg\Queue\Console\RunQueue;
 use Pckg\Queue\Console\RunRabbitTask;
 use Pckg\Queue\Console\RunRabbitWorker;
-use Pckg\Queue\Controller\Queue as QueueController;
 use Pckg\Queue\Service\RabbitMQ;
 
 class Queue extends Provider
 {
-
-    public function assets()
-    {
-        return [
-            //'footer' => [
-            //'js/index.js',
-            //],
-        ];
-    }
-
-    public function providers()
-    {
-        return [
-            ManagerProvider::class,
-            DynamicProvider::class,
-            AuthProvider::class,
-            GenericProvider::class,
-        ];
-    }
-
-    public function routes()
-    {
-        return [
-            'url' => [
-                '/'                 => [
-                    'controller' => QueueController::class,
-                    'view'       => 'index',
-                    'name'       => 'pckg.queue.index',
-                    'tags'       => ['group:admin'],
-                ],
-                '/jobs'             => [
-                    'controller' => QueueController::class,
-                    'view'       => 'index',
-                    'name'       => 'pckg.queue.index',
-                    'tags'       => ['group:admin'],
-                ],
-                '/ajax/jobs/[type]' => [
-                    'controller' => QueueController::class,
-                    'view'       => 'ajax',
-                    'name'       => 'pckg.queue.ajax.jobs',
-                    'tags'       => ['group:admin'],
-                ],
-            ],
-        ];
-    }
 
     public function consoles()
     {
